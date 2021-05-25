ba769e496d9b02b0dd3683ff9beee80a3b895422
Sploters <mrsploters@outlook.com>
2021-05-25 10:38:15 -0300

    Master Commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("June 24, 2021 00:00:00").getTime();
+    const countDate = new Date("May 24, 2022 21:00:00").getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
@@ -20,4 +20,4 @@ const countdown = () => {
 
 };
 
-setInterval(countdown, 1000);
+setInterval(countdown, 10);
